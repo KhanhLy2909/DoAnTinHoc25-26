17117df778e20e3b28cb7a5163f5c0c560cac9ac
KhanhLy <nguyenkhanhly0929@gmail.com>
2025-10-25 19:21:20 +0700

    them code doc ghi

---
+++ DoAnTinHoc_Ly_Winf/Form1.Designer.cs
@@ -0,0 +1,49 @@
+﻿namespace CsvReaderWriter
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnRead;
+        private System.Windows.Forms.Button btnSave;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnRead = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.Location = new System.Drawing.Point(12, 60);
+            this.dataGridView1.Size = new System.Drawing.Size(760, 380);
+            this.dataGridView1.TabIndex = 0;
+            this.btnRead.Location = new System.Drawing.Point(12, 12);
+            this.btnRead.Size = new System.Drawing.Size(100, 30);
+            this.btnRead.Text = "Đọc file CSV";
+            this.btnRead.Click += new System.EventHandler(this.btnRead_Click);
+            this.btnSave.Location = new System.Drawing.Point(130, 12);
+            this.btnSave.Size = new System.Drawing.Size(100, 30);
+            this.btnSave.Text = "Lưu TXT";
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            this.ClientSize = new System.Drawing.Size(784, 461);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnRead);
+            this.Controls.Add(this.dataGridView1);
+            this.Text = "Đọc và lưu file CSV";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+        }
+    }
+}
---
+++ DoAnTinHoc_Ly_Winf/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Data;
+using System.IO;
+using System.Windows.Forms;
+
+namespace CsvReaderWriter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRead_Click(object sender, EventArgs e)
+        {
+            string filePath = "Reading Habbit Of Students.csv";
+            if (!File.Exists(filePath))
+            {
+                MessageBox.Show("Không tìm thấy file CSV trong thư mục bin\\Debug!");
+                return;
+            }
+
+            DataTable dt = new DataTable();
+            string[] lines = File.ReadAllLines(filePath);
+
+            if (lines.Length > 0)
+            {
+                string[] headers = lines[0].Split(',');
+                foreach (string header in headers)
+                {
+                    dt.Columns.Add(header.Trim());
+                }
+                for (int i = 1; i < lines.Length; i++)
+                {
+                    string[] values = lines[i].Split(',');
+                    while (values.Length < dt.Columns.Count)
+                    {
+                        Array.Resize(ref values, dt.Columns.Count);
+                    }
+                    if (values.Length > dt.Columns.Count)
+                    {
+                        Array.Resize(ref values, dt.Columns.Count);
+                    }
+
+                    dt.Rows.Add(values);
+                }
+
+                dataGridView1.DataSource = dt;
+            }
+        }
+
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            string filePath = "output.txt"; 
+            using (StreamWriter sw = new StreamWriter(filePath))
+            {
+                for (int i = 0; i < dataGridView1.Columns.Count; i++)
+                {
+                    sw.Write(dataGridView1.Columns[i].HeaderText);
+                    if (i < dataGridView1.Columns.Count - 1)
+                        sw.Write("\t");
+                }
+                sw.WriteLine();
+                foreach (DataGridViewRow row in dataGridView1.Rows)
+                {
+                    if (!row.IsNewRow)
+                    {
+                        for (int i = 0; i < row.Cells.Count; i++)
+                        {
+                            sw.Write(row.Cells[i].Value);
+                            if (i < row.Cells.Count - 1)
+                                sw.Write("\t");
+                        }
+                        sw.WriteLine();
+                    }
+                }
+            }
+
+            MessageBox.Show("Đã lưu dữ liệu thành công ra file output.txt!");
+        }
+    }
+}
